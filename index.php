5d1cceb60633ac23b07c2f10cf8001484bec25b0
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-10-13 07:01:13 -0700

    create new router

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
 <?php 
-
-
-
    require('functions.php');
-   $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-   
-   $routes = [
-    '/' => 'controllers/index.php',
-    '/staff' =>'controllers/staff.php',
-    '/statistics' =>'controllers/statistics.php',
-    '/about' => 'controllers/about.php',
-   ];
-
-   function abort($code) {
-    http_response_code($code);
-    require "view/{$code}.php";
-   }
-    if(array_key_exists($uri,$routes)){
-        require $routes[$uri];
-    }
-    else{
-       abort();
-    }
+require('router.php');
