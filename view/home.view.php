f2b054ef18fa9dd239a7f7f15d3c8e05328fb8b5
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-10-13 02:54:07 -0700

    change the curser to pointer

--- view/home.view.php
+++ view/home.view.php
@@ -4,8 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href ="style.css" >
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Mr+Dafoe&display=swap" rel="stylesheet">
 </head>
 <body>
-    <p>the more you take </p>
+    <header class ="header-main">
+        <div class ="header-logo">
+            <p>_strategy</p>
+            <nav class = "header-nav">
+        <ul>
+         <li>   <a href = "#"></a>      </li>
+         <li>   <a href = "#"></a>      </li>
+         <li>   <a href = "#"></a>      </li>
+        </ul>
+        
+     </nav>
+        </div>
+    
+     <div class="header-sm"></div>
+    </header>
 </body>
 </html>
