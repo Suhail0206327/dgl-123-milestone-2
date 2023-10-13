5d8f3d0f2c841805d8c316f5fbb2aa955ced257c
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-10-13 03:06:42 -0700

    add margin to the nav

--- view/home.view.php
+++ view/home.view.php
@@ -15,9 +15,10 @@
             <p>_strategy</p>
             <nav class = "header-nav">
         <ul>
-         <li>   <a href = "#"></a>      </li>
-         <li>   <a href = "#"></a>      </li>
-         <li>   <a href = "#"></a>      </li>
+         <li>   <a href = "#">home</a>      </li>
+         <li>   <a href = "#">staff</a>      </li>
+         <li>   <a href = "#">statistics</a>      </li>
+         <li>   <a href = "#">about</a>      </li>
         </ul>
         
      </nav>
