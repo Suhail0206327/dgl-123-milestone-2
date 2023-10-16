965ed5ced04fe6190c2be14263414e02d00c2899
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-10-15 19:54:53 -0700

    create the box shadow

--- view/staff.view.php
+++ view/staff.view.php
@@ -1,7 +1,27 @@
 <?php require('partials/header.php');?>
        
        
-    <main>
-<?php require('partials/banner.php');?>
+    <main class="staff">
+        <h1>Total number of employees: 256</h1>
+        <table>
+            <th>FOH</th>
+            <tr>
+                <td >
+                    <div class="face"></div>
+                </td>
+                <td>name</td>
+                <td>name</td>
+                <td>name</td>
+            </tr>
+            <tr>
+                <td >
+                    <div class="face"></div>
+                </td>
+                <td>name</td>
+                <td>name</td>
+                <td>name</td>
+            </tr>
+        </table>
+        
     </main>
     <?php require('partials/footer.php');?>
