f3b572cc9c852642adc24c968cebd96b665fb1b2
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-10-15 22:38:44 -0700

    create a calendar

--- view/statistics.view.php
+++ view/statistics.view.php
@@ -3,7 +3,9 @@
        
     <main class = "statistics">
 <h1><?=$currentDate?></h1>
-
+<div class = "calendar">
 <?= generate_calendar($currentMonth, $currentYear); ?>
+</div>
+
     </main>
     <?php require('partials/footer.php');?>
