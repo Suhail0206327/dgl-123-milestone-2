c49de08d42fc0ef5f6afe14fe3af30a3395a38f8
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-08 13:11:45 -0800

    add form and heading to login page

--- view/login.view.php
+++ view/login.view.php
@@ -18,8 +18,14 @@
   <div></div>
   <p>Back to website</p>
   </button>
+  <h1>Welcome!</h1>
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form>
 </div>
-<h1>Welcome!</h1>
 
   </main>
 </body>
