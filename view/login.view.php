c8e00edca386c1846e215d4b0adf9b9d35fd5ddd
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-08 13:09:45 -0800

    style the back button in login page

--- view/login.view.php
+++ view/login.view.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href ="style.css" >
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
@@ -12,11 +13,13 @@
 
 <img src="assets\harrypotter.jpg">
 <div class="login-form">
-  <buton>
+  <button>
   <i class="fa-solid fa-chevron-left"></i>
+  <div></div>
   <p>Back to website</p>
-  </buton>
+  </button>
 </div>
+<h1>Welcome!</h1>
 
   </main>
 </body>
