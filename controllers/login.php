e872c574b47faed75e9faceb6fafd0ad7df80570
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-24 15:59:29 -0800

    move the post method to login controller

--- controllers/login.php
+++ controllers/login.php
@@ -1,6 +1,12 @@
 <?php
 
 $config = require('config.php');
-$db = new Database($config,'root', 'suhail@1');
+$db = new Database($config);
 
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    $db->query('INSERT INTO users( email , password) VALUES (:email, :password)', [
+      'email' => $_POST['email'],
+      'password' => $_POST['password'],
+    ]);
+   }
 require ('view/login.view.php');
