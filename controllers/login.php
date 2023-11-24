7ed2cd3d6a3dba949c032a9d7b98a116c10f8265
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-24 15:57:50 -0800

    update the password of the database

--- controllers/login.php
+++ controllers/login.php
@@ -1,2 +1,6 @@
 <?php
+
+$config = require('config.php');
+$db = new Database($config,'root', 'suhail@1');
+
 require ('view/login.view.php');
--- view/login.view.php
+++ view/login.view.php
@@ -1,7 +1,7 @@
 <?php 
 
 $config = require('config.php');
-$db = new Database($config,'root', 'suhail@1');
+$db = new Database($config);
  if ($_SERVER['REQUEST_METHOD']=='POST'){
   $db->query('INSERT INTO users( email , password) VALUES (:email, :password)', [
     'email' => $_POST['email'],
