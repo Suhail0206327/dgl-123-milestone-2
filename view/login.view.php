cd524d756209b3e916695eb3fe08167004ecddca
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-24 16:03:13 -0800

    remove the database config from the login view file

--- view/login.view.php
+++ view/login.view.php
@@ -1,13 +1,7 @@
 <?php 
 
-$config = require('config.php');
-$db = new Database($config);
- if ($_SERVER['REQUEST_METHOD']=='POST'){
-  $db->query('INSERT INTO users( email , password) VALUES (:email, :password)', [
-    'email' => $_POST['email'],
-    'password' => $_POST['password'],
-  ]);
- }
+
+
  ?>
 <!DOCTYPE html>
 <html lang="en">
