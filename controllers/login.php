bbf9c5e031eb2998df5ed965babc22a2ee8b05d2
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-25 01:31:19 -0800

    prevent login is the user already exits

--- controllers/login.php
+++ controllers/login.php
@@ -3,10 +3,16 @@ require 'validator.php';
 $config = require('config.php');
 $db = new Database($config);
 
-$errors = [];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$users = $db->query('select * from users where email = :email',[
+    'email' => $email]);
+    
+$errors = [] ;
 if($_SERVER['REQUEST_METHOD']== 'POST') {
 
     if(!Validator::password($_POST['password'])) {
+        
         $errors['body'] = 'The password should be atleast 6 and less than 10 characters';
         }
     
@@ -16,16 +22,35 @@ if($_SERVER['REQUEST_METHOD']== 'POST') {
       
         
         if (empty($errors)) {
-            if ($_SERVER['REQUEST_METHOD']=='POST'){
-                $db->query('INSERT INTO users( email , password) VALUES (:email, :password)', [
-                  'email' => $_POST['email'],
-                  'password' => $_POST['password'],
-                ]);
-               }
+            $users = $db->query('select * from users where email = :email',[
+                'email' => $email
+            ])->find();
             
+            if ($users) {
+                
+            }
+            else {
+                $db->query('INSERT INTO users( email , password) VALUES (:email, :password)', [
+                    'email' => $email,
+                    'password' =>$password,
+                  ]);    
+                
+                  
+                  $_SESSION ['user'] = [
+                    'email' => $email
+                  ];
+            }
         }
+        
 }
 
+
+
+
+// $db->query('INSERT INTO users( email, password ) VALUES(:email, :password)',[
+//     'email' => $email,
+//     'password' => $password,
+// ]);
 
 
 require ('view/login.view.php');
