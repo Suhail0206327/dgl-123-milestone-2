be3bfc41dfb6184de399f6834d13255408346f56
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-24 16:40:00 -0800

    validate the password input
    here the user should submit a password between 8 and 10 characters

--- controllers/login.php
+++ controllers/login.php
@@ -3,10 +3,23 @@
 $config = require('config.php');
 $db = new Database($config);
 
-if ($_SERVER['REQUEST_METHOD']=='POST'){
-    $db->query('INSERT INTO users( email , password) VALUES (:email, :password)', [
-      'email' => $_POST['email'],
-      'password' => $_POST['password'],
-    ]);
-   }
+$errors = [];
+if(strlen($_POST['password']) < 8) {
+$errors['body'] = 'The password should be atleast 8 characters';
+}
+if(strlen($_POST['password'])>10) {
+    $errors['body'] = 'The password should be less than 10 characters';
+}
+
+if (empty($errors)) {
+    if ($_SERVER['REQUEST_METHOD']=='POST'){
+        $db->query('INSERT INTO users( email , password) VALUES (:email, :password)', [
+          'email' => $_POST['email'],
+          'password' => $_POST['password'],
+        ]);
+       }
+    
+}
+
+
 require ('view/login.view.php');
