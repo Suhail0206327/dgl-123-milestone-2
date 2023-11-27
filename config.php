b11d892132d6af05a94a5c0139cc273ad04a0e57
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-26 23:10:13 -0800

    change the database name

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ return
    
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'strategym3',
+        'dbname' => 'n0206327_strategym3',
         'charset' => 'utf8mb4',
         'password' => 'suhail@1'
 
