42d199a48ad0d4c2c9ba6893f29abcef98e88697
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2023-11-26 16:10:43 -0800

    link to database

--- controllers/staff_create.php
+++ controllers/staff_create.php
@@ -1,4 +1,26 @@
 <?php
 $config = require('config.php');
 $db = new Database($config);
+
+if ($_SERVER['REQUEST_METHOD']== 'POST') {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $position = $_POST['positon']; 
+    $wage = $_POST['pay'];
+    $file_name = $_FILES["image"]["name"];
+    $file_tmp = $_FILES["image"]["tmp_name"];
+    $image = file_get_contents($file_tmp);
+    $db->query('INSERT INTO kitchen( name ,positon, pay,image, email  ) VALUES (:name, :positon,  :pay,  :image,:email,)', [
+      'name' => $name,
+      'positon' =>$position,
+      'pay' => $wage,
+      'image' => $image,
+        'email' => $email,
+      
+       
+      ]);    
+    
+}
+
 require 'view/create_staff.view.php';
--- view/create_staff.view.php
+++ view/create_staff.view.php
@@ -16,7 +16,7 @@
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6" action="#" method="POST" enctype="multipart/form-data">
       <div>
         <label for="name" class="block text-sm font-medium leading-6 text-gray-900">name</label>
         <div class="mt-2">
@@ -30,11 +30,11 @@
           
         </div>
         <div class="mt-2">
-          <select id="cars" name="cars" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
+          <select id="positon" name="positon" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <option value="chef">Chef</option>
+            <option value="sous_chef">Sous Chef</option>
+            <option value="linecook">Line Cook</option>
+            <option value="prep/dish">Prep/Dish</option>
         </select>
         </div>
       </div>
@@ -42,7 +42,7 @@
       <div>
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">email</label>
         <div class="mt-2">
-          <input id="email" name="name" type="email"  required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input id="email" name="email" type="email"  required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
       </div>
 
@@ -61,7 +61,7 @@
       </div>
 
       <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</button>
+        <button type="submit" name = "submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</button>
       </div>
     </form>
 
